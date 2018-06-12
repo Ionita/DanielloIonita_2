a1cee4155b4ab0af0b8c1ebe8dde44dd0cc85e70
Marius Dragos Ionita <ionita.maryus@gmail.com>
2018-06-12 16:25:23 +0200

    alèèè

---
+++ src/main/java/controllers/DataReader.java
@@ -0,0 +1,36 @@
+package controllers;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import entities.Friend;
+import entities.Message;
+
+import java.util.ArrayList;
+
+public class DataReader {
+
+
+    public  DataReader() {}
+
+    public static ArrayList<Friend> getData(String jsonString){
+
+
+        ArrayList<Friend> messages = new ArrayList<>();
+        JsonParser parser = new JsonParser();
+        JsonElement element = parser.parse(jsonString);
+        if (element.isJsonObject()) {
+            JsonObject jsonRecord = element.getAsJsonObject();
+            Friend data = new Friend();
+            data.setUser_1(jsonRecord.get("user_id1").getAsInt());
+            data.setUser_2(jsonRecord.get("user_id2").getAsInt());
+
+            messages.add(data);
+        } else {
+            System.out.println("Not a valid Json Element");
+        }
+
+
+        return messages;
+    }
+}
